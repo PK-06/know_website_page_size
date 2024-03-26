335aa50788a93f410d31f4058ab8e90a2d7a048b
PK-06 <165055628+PK-06@users.noreply.github.com>
2024-03-26 17:33:30 +0100

    script.js

---
+++ source/script.js
@@ -0,0 +1,7 @@
+function size() {
+  	var w = window.innerWidth;
+	  var h = window.innerHeight;
+
+		var x = document.getElementById("result");
+		x.innerHTML = "width: " + w + "px , height: " + h + "px .";
+}
